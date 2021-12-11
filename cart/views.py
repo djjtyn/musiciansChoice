2f15ffe580d66546a58d1160ef00928c459a71b4
EC2 Default User <ec2-user@ip-172-31-46-110.eu-west-1.compute.internal>
2021-12-11 18:30:34 +0000

    cart adjusting quantaties for existing instruments

--- cart/views.py
+++ cart/views.py
@@ -8,6 +8,7 @@ import json
 # Display cart contents
 def view_cart(request):
     cart = request.session.get('cart', {})
+    print(cart)
     return render(request, 'cart.html')
     
 # Empty cart contents
@@ -21,10 +22,11 @@ def add_to_cart(request, instrument_id):
     quantity = int(request.POST.get('quantity'))
     #If no cart exists, create a cart, else retrieve existing cart
     cart = request.session.get('cart',{})
-    cart[instrument_id] = quantity
-    # # Check if item is already in cart
-    # if quantity > 0:
-    #     cart[instrument_id] = quantity
+    # Determine if the instrument is already in the cart, add to it's quantity if it is
+    if str(instrument_id) in cart:
+        cart[str(instrument_id)] += quantity
+    else:
+        cart[instrument_id] = quantity
     request.session['cart'] = cart
     messages.success(request, "Your cart is now empty")
     return render(request, 'cart.html')

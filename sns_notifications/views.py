0695b2c3734e06c1e0a8d3d16b79fbc68f0e0484
EC2 Default User <ec2-user@ip-172-31-46-110.eu-west-1.compute.internal>
2021-12-12 23:13:27 +0000

    sns subscriptions implemented

--- sns_notifications/views.py
+++ sns_notifications/views.py
@@ -3,18 +3,16 @@ import boto3
 from django.contrib import messages
 import traceback
 from django.contrib.auth.decorators import login_required
+from .sns_utils import sns
 
 # Login required to be notified on stock return for products
 @login_required()
 def notify_when_product_is_back_in_stock(request, instrument_id):
     # Create a topic if it doesn't exist already, if it does exist get the topics ARN
     try:
-        sns_client = boto3.client('sns')
-        response = sns_client.create_topic(Name=f"stockNotificationForInstrumentid{instrument_id}")
-        topic_arn = response['TokenArn']
-        userEmail = request.user
-        subscription = sns_client.subscribe(TopicArn=topic_arn, Protocol='email', Endpoint = request.user, ReturnSubscriptionArn = True)
-        messages.info(request, f"{userEmail} will be notified when stock returns")
+        user_email = request.user.email
+        sns.topic_subscribe(f"restockNotificationForInstrumentId{instrument_id}", user_email)
+        messages.info(request, f"An email was sent to {user_email} to confirm signing up to receive a notification when this product is back in stock")
     except:
         messages.info(request, "An error occured processing this request")
         print(traceback.format_exc())

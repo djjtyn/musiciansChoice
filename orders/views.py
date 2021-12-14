f2712882c4021ef539dcf2cfb272070b8ebe42ae
EC2 Default User <ec2-user@ip-172-31-46-110.eu-west-1.compute.internal>
2021-12-14 02:21:46 +0000

    Custom Order history dispalying for admins and geenric users

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Order
 
 # Create your views here.
+def view_orders(request):
+    # If the user is a admin, display all others
+    if request.user.is_admin:
+        try:
+            # Get all the orders from the database
+            orders = Order.objects.all()
+            print(orders)
+        except:
+            print("Issue retrieving the orders")
+    else:
+        # Get all the orders linked to the logged in user
+        orders = Order.objects.filter(customer = request.user)
+        print("Is not admin")
+    return render(request, 'orders.html', {'orders': orders})

99f4acf9c469d72786d2adaada857a8b7100de5c
EC2 Default User <ec2-user@ip-172-31-35-172.eu-west-1.compute.internal>
2021-12-16 13:37:30 +0000

    check for aws credentials for eb

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from instruments.models import Instrument
+from django.conf import settings
 
 # Method to display the application home page
 def display_home_page(request):
+    access_key = settings.AWS_ACCESS_KEY_ID
     # Gather recommendations
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'key': access_key})
